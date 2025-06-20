67ad62373266ea86ef386f705070fb37bdb69f98
tomo-local <tomohiro.togashi@gmail.com>
2025-06-21 01:15:45 +0900

    chore(main): remove commented code and improve readability

--- service/memo/app/cmd/server/main.go
+++ service/memo/app/cmd/server/main.go
@@ -15,29 +15,23 @@ import (
 )
 
 func main() {
-	// 1. 8080番portのListenerを作成
 	port := 8080
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		panic(err)
 	}
 
-	// 2. gRPCサーバーを作成
 	s := grpc.NewServer()
 
-	// 2-1. 作成したgRPCサーバーに、サービスを登録する
 	pb.RegisterMemoServiceServer(s, service.NewMemoService())
 
-	// 2-2. gRPCサーバーに、Reflectionを登録する
 	reflection.Register(s)
 
-	// 3. 作成したgRPCサーバーを、8080番ポートで稼働させる
 	go func() {
 		log.Printf("start gRPC server port: %v", port)
 		s.Serve(listener)
 	}()
 
-	// 4.Ctrl+Cが入力されたらGraceful shutdownされるようにする
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
