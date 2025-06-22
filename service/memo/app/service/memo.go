e1c9a7d42dbc94af0a0690bbce19c916144eba24
tomo-local <tomohiro.togashi@gmail.com>
2025-06-22 22:28:22 +0900

    feat(memo): CreateMemoメソッドを追加し、メモの作成機能を実装

--- service/memo/app/service/memo.go
+++ service/memo/app/service/memo.go
@@ -1,13 +1,19 @@
 package service
 
 import (
+	config "memo/config/server"
+	"memo/db"
 	pb "memo/grpc"
 )
 
 type MemoService struct {
 	pb.UnimplementedMemoServiceServer
+	db.FileService
 }
 
-func NewMemoService() *MemoService {
-	return &MemoService{}
+func NewMemoService(env *config.Config) *MemoService {
+	fs, _ := db.GetService(env)
+	return &MemoService{
+		FileService: fs,
+	}
 }
